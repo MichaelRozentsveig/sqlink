50b86e2ca17e476deafd4be29a62f1727e57b0d5
MichaelRozentsveig <46834545+MichaelRozentsveig@users.noreply.github.com>
2019-08-12 11:05:46 +0300

    Add files via upload

---
+++ C/09_StrPointers/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Header.h"
+
+typedef int l;
+
+int main(){
+	
+	int capacity=0;
+	int* num=0;
+	struct DA_t* dynArrSt = NULL;
+	
+	printf("Enter Size of Array:\n");
+	scanf("%d",&capacity);
+
+	dynArrSt = createDA(capacity); /*Creates new struct with new array (size of capacity)*/
+
+	while(1){
+	
+		printf("Insert number: (-1 to exit)\n");
+		num = malloc(sizeof(int));		
+		scanf("%d",num);
+		if (*num==-1) {break;}
+		insert(dynArrSt, num); /* adding num to the array*/
+	}
+	printf("\n");
+
+	printDA(dynArrSt); /*Printing the array */
+	
+	destroy(dynArrSt); /*Free array and struct*/
+}
+
+
